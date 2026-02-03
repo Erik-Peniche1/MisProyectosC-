228350da3491afc8d54ccefa9d59434ff4f48475
Erik-Peniche1 <peniche.gomez.erik.p6@gmail.com>
2026-02-02 21:44:19 -0600

    Actividad del triángulo finalizada

---
+++ AreaTriangulo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AreaTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Calculadora de Área de Triángulo ---");
+            
+            Console.Write("Ingresa la base: ");
+            double baseT = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Ingresa la altura: ");
+            double alturaT = Convert.ToDouble(Console.ReadLine());
+            
+            double area = (baseT * alturaT) / 2;
+            
+            Console.WriteLine($"\nEl área es: {area}");
+        }
+    }
+}
